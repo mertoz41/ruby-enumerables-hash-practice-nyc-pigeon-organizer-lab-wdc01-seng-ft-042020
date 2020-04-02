17c33c559a80ecafe212efecf5df3e896989584f
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-02 22:56:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+   
+    value.each do |prop, names|
+ 
+      names.each do  |name|
+ 
+        if !new_hash[name] 
+        
+          new_hash[name] = {}
+        end 
+        if !new_hash[name][key]
+          
+          new_hash[name][key] = []
+          
+      end
+      binding.pry 
+      new_hash[name][key] << prop.to_s 
+    end 
+  end 
+  end 
+  new_hash
 end
